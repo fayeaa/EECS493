97598fe6c830753b0004af9ddfdc7e3056caca0e
corneliahd <hudie@umich.edu>
2016-12-15 00:28:58 -0500

    events.js

---
+++ events.js
@@ -0,0 +1,45 @@
+
+var app = angular.module('EventOnCampus', []);
+app.controller('ListEvent',function($scope){
+    $scope.UMevents = recent_event;
+    $scope.numEvent = function(){
+    	return $scope.numEvents.length;
+    }
+    console.log("1");
+    console.log($scope.numEvent);
+});
+
+var recent_event = [
+{
+	"name": "Umix",
+	"disc": "Enjoy Harry Potter themed games and crafts",
+	"place": "Michigan Union,, Grand Ballroom",
+	"date": "Friday, November 6th",
+	"time": "9pm-11pm",
+	"category": "entertainment"
+},
+{
+	"name": "G-Men Concert",
+	"disc": "Michigan's finest men's acappella group live",
+	"place": "Rackham Auditorium",
+	"date": "Saturday, November 7th",
+	"time": "6:30pm-8pm",
+	"category": "music"
+},
+{
+	"name": "TedX U of M",
+	"disc": "Hear great speakers share their stories",
+	"place": "Hill Auditorium",
+	"date": "Thursday, November 12th",
+	"time": "4pm-9pm",
+	"category": "acedemic"
+},
+{
+	"name": "Men's Basketball",
+	"disc": "Woverines take on the bobcats at home",
+	"place": "Crisier Arena",
+	"date": "Thursday, November 12th",
+	"time": "7pm-9pm",
+	"category": "sports"
+}
+];
