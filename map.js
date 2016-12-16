26cdc0aae888200291727585d46402a189a9b257
czhang0516 <clzhang@umich.edu>
2016-12-16 15:05:27 -0500

    Add files via upload

--- map.js
+++ map.js
@@ -2,10 +2,76 @@
 var map;
 var input;
 var searchBox;
+var test1;
+var test2;
+
+var app = angular.module('EventOnCampus', []);
+app.controller('ListEvent',function($scope){
+    $scope.UMevents = recent_event;
+    $scope.numEvent = function(){
+    	return $scope.numEvents.length;
+    }
+    $scope.category = ["", "entertainment", "music", "sports", "academic"];
+    console.log("1");
+    console.log($scope.numEvent);
+    $scope.searchMap=function(place){
+      //$scope.isActive = !$scope.isActive;
+      console.log(place);
+      console.log($scope.mapSearchbox);
+
+
+  }
+});
+app.controller('mapCtrl',function($scope,$interval){
+    $scope.placeTitle=test1;
+    $scope.placePosition=test2;
+    $interval(function () {
+        $scope.placeTitle = test1;
+        $scope.placePosition=test2;
+    }, 100);
+});
+
+
+var recent_event = [
+{
+	"name": "Umix",
+	"disc": "Enjoy Harry Potter themed games and crafts",
+	"place": "Michigan Union, Grand Ballroom",
+	"date": "Friday, November 6th",
+	"time": "9pm-11pm",
+	"category": "entertainment"
+},
+{
+	"name": "G-Men Concert",
+	"disc": "Michigan's finest men's acappella group live",
+	"place": "Rackham Auditorium",
+	"date": "Saturday, November 7th",
+	"time": "6:30pm-8pm",
+	"category": "music"
+},
+{
+	"name": "TedX U of M",
+	"disc": "Hear great speakers share their stories",
+	"place": "Hill Auditorium",
+	"date": "Thursday, November 12th",
+	"time": "4pm-9pm",
+	"category": "academic"
+},
+{
+	"name": "Men's Basketball",
+	"disc": "Woverines take on the bobcats at home",
+	"place": "Crisier Arena",
+	"date": "Thursday, November 12th",
+	"time": "7pm-9pm",
+	"category": "sports"
+}
+];
+
 
 $(document).ready( function() {
-	console.log("sanity check")
-	
+	console.log("sanity check");
+
+
 });
 
 
@@ -65,6 +131,20 @@ function initMap() {
         title: place.name,
         position: place.geometry.location
       }));
+      //show pop-out window
+      test1=markers[0].title;
+      test2=markers[0].position;
+
+      var infowindow = new google.maps.InfoWindow({
+        content:markers[0].title,
+        position: place.geometry.location
+        });
+
+        google.maps.event.addListener(markers[0], 'click', function() {
+          infowindow.open(map,markers[0]);
+          $('#myModal').modal('show');
+
+        });
 
       if (place.geometry.viewport) {
         // Only geocodes have viewport.
